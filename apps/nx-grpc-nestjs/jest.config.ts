3fabf44b05d82dc49235fdd153d6452c0bde0dc1
dkokane0 <dkokane0@gmail.com>
2023-01-02 15:38:35 +0530

    Initial commit

---
+++ apps/nx-grpc-nestjs/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx-grpc-nestjs',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nx-grpc-nestjs',
+};
